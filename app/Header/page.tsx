d0935a2e858cc5c97e33b9e32a13e5e35ad39a40
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2025-10-06 19:22:53 +0300

    The home section has been updated.

--- app/Header/page.tsx
+++ app/Header/page.tsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
 import { HiOutlineDownload } from "react-icons/hi";
-import { FaLinkedinIn } from "react-icons/fa";
+import { BiLogoLinkedin } from "react-icons/bi";
 import { FaGithub } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md";
 import Image from "next/image";
 import Profile from "@/public/Profile.jpg";
 export default function Header() {
   return (
-    <header className="h-[100vh] text-white py-12 relative">
+    <header className="h-[100vh] text-white py-12 relative ">
       <div className="flex pt-10 flex-col lg:flex-row items-center justify-between px-[8%] lg:px-[16%]">
         <div className="lg:w-1/2 text-start md:text-left">
-          <p className="text-lg mb-2 text-gray-400">
+          <p className="text-lg mb-2 text-[var(--text-color)]">
             MIS Graduate | Bridging Business & Technology
           </p>
-          <h1 className="text-7xl text-start lg:text-8xl mb-2 text-purple-400 ">
-            Hello I'm <span className="text-purple-700">Kader Bozkaya</span>
+          <h1 className="text-7xl text-start lg:text-8xl mb-2 text-[var(--text-color)] ">
+            Hello I'm{" "}
+            <span className="text-[var(--text-color2)]">Kader Bozkaya</span>
           </h1>
           <p className="text-[color:var(--text-color)] text-md lg:text-xl my-8">
             Passionate about transforming business needs into impactful digital
@@ -23,13 +24,13 @@ export default function Header() {
           <div className="flex flex-wrap gap-8 items-center">
             <Link
               href="#"
-              className="border border-purple-500 font-bold text-[var(--text-color)] px-6 py-3 rounded hover:bg-[var(--text-color)] hover:text-white transition-all duration-500 flex items-center "
+              className="border border-[var(--text-color2)] font-bold  px-6 py-3 rounded hover:bg-[var(--text-color2)] hover:text-white transition-all duration-500 flex items-center "
             >
-              <HiOutlineDownload className="text-xl me-2" /> Download CV
+              <HiOutlineDownload className="text-2xl me-2" /> Download CV
             </Link>
 
             <div className="flex hero-social gap-2 text-xl">
-              <FaLinkedinIn className="hero-social-icon" />
+              <BiLogoLinkedin className="hero-social-icon" />
               <FaGithub className="hero-social-icon" />
               <MdAlternateEmail className="hero-social-icon" />
             </div>
@@ -37,7 +38,7 @@ export default function Header() {
         </div>
         <div className="lg:w-1/2 w-full mt-10 lg:mt:0 flex justify-center relative">
           <div className="relative rounded-full flex items-center justify-center">
-            <div className="relative hero-image w-full h-full rounded-full overflow-hidden bg-gradient-to-r from-[var(--back-color)] to-[var(--text-color)]">
+            <div className="relative hero-image w-full h-full rounded-full overflow-hidden">
               <Image
                 src={Profile}
                 alt="Profile picture"
--- app/Nav.tsx
+++ app/Nav.tsx
@@ -20,7 +20,7 @@ export default function Nav() {
   ];
 
   return (
-    <nav className="w-full z-[99999] transition-all duration-300 relative">
+    <nav className="w-full z-[99999] transition-all duration-300 relative ">
       <div className="flex justify-between items-center px-[8%] lg:px-[16%] py-6">
         <Link href="/">
           <Image src={logo} alt="logo" className="w-28 cursor-pointer mr-14" />
@@ -32,7 +32,7 @@ export default function Nav() {
             <Link
               key={link.name}
               href={link.href}
-              className={`text-base font-bold transition-all text-purple-800 relative px-2 py-2 rounded hover:text-[--text-color] ${
+              className={`text-base font-bold transition-all  relative px-2 py-2 rounded hover:text-[--text-color] ${
                 pathname === link.href
                   ? "active-links text-[--text-color-dark]"
                   : ""
@@ -69,7 +69,7 @@ export default function Nav() {
 
       {/* Mobile Menu */}
       <div
-        className={`lg:hidden absolute left-0 top-full w-full bg-blue-800 text-white overflow-hidden transition-all duration-500 ease-in-out ${
+        className={`lg:hidden absolute left-0 top-full w-full bg-[var(--back-color)] text-white overflow-hidden transition-all duration-500 ease-in-out ${
           isMenuOpen ? "max-h-96 py-5 opacity-100" : "max-h-0 py-0 opacity-0"
         }`}
       >
