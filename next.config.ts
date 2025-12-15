4d8d1cd05685feb58a47ed0cfea146b62370b978
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2025-12-15 19:34:08 +0300

    chore: github pages config

--- next.config.ts
+++ next.config.ts
@@ -6,15 +6,28 @@
 
 // export default nextConfig;
 
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export", 
+//   images: {
+//     unoptimized: true,
+//   },
+//   basePath: "/portfolio",      // ⬅️ BURAYA REPO ADINI YAZ
+//   assetPrefix: "/portfolio/",   // ⬅️ BURAYA DA AYNI
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", 
+  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/portfolio",      // ⬅️ BURAYA REPO ADINI YAZ
-  assetPrefix: "/portfolio/",   // ⬅️ BURAYA DA AYNI
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
